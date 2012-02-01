8483cbac107807f4b7d6ec9eb4240178fa128719
Boyu Guo <iambus@gmail.com>
2012-02-01 17:49:55 +0800

    init project

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject cljasm "0.0.1-SNAPSHOT"
+  :description "ASM API for Clojure"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/cljasm/test/core.clj
@@ -0,0 +1,6 @@
+(ns cljasm.test.core
+  (:use [cljasm.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
